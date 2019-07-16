a833874af2bf33b8607ecd7c37db98c9abb53add
sralloza <sralloza@gmail.com>
2019-07-16 12:02:43 +0200

    Add warnings for errors with sudoers file

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,20 @@
 import json
+import warnings
 
 from .config import config
+
+
+class SudoersWarning(Warning):
+    pass
 
 
 def get_sudoers():
     with config.SUDOERS_PATH.open() as f:
         try:
             return json.load(f)
-        except (json.JSONDecodeError, FileNotFoundError):
+        except json.JSONDecodeError as err:
+            warnings.warn(f'json decode error: {err}', SudoersWarning)
+            return []
+        except FileNotFoundError:
+            warnings.warn('Sudoers file not found', SudoersWarning)
             return []
