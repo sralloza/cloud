6cb283f7d86fd42296eb1a1fb3342b6335163953
sralloza <sralloza@gmail.com>
2019-07-17 12:53:23 +0200

    Test utils

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,7 @@
 import json
 import warnings
 
-from .config import config
+from .config import cfg
 
 
 class SudoersWarning(Warning):
@@ -9,12 +9,12 @@ class SudoersWarning(Warning):
 
 
 def get_sudoers():
-    with config.SUDOERS_PATH.open() as f:
-        try:
+    try:
+        with cfg.SUDOERS_PATH.open() as f:
             return json.load(f)
-        except json.JSONDecodeError as err:
-            warnings.warn(f'json decode error: {err}', SudoersWarning)
-            return []
-        except FileNotFoundError:
-            warnings.warn('Sudoers file not found', SudoersWarning)
-            return []
+    except json.JSONDecodeError as err:
+        warnings.warn(f'json decode error: {err}', SudoersWarning)
+        return []
+    except FileNotFoundError:
+        warnings.warn('Sudoers file not found', SudoersWarning)
+        return []
---
+++ tests/test_utils.py
@@ -0,0 +1,26 @@
+import warnings
+from unittest import mock
+
+import pytest
+
+from app.utils import SudoersWarning, get_sudoers
+
+
+def test_warning():
+    with pytest.warns(SudoersWarning):
+        warnings.warn('Sudoers Warning', SudoersWarning)
+
+
+def test_get_sudoers():
+    with mock.patch('app.utils.cfg') as mocker:
+        fp = mocker.SUDOERS_PATH.open.return_value.__enter__.return_value
+        fp.read.return_value = '["user_a"]'
+        assert get_sudoers() == ['user_a']
+
+        with pytest.warns(SudoersWarning, match='json decode error'):
+            fp.read.return_value = 'invalid'
+            assert get_sudoers() == []
+
+        with pytest.warns(SudoersWarning, match='Sudoers file not found'):
+            fp.read.side_effect = FileNotFoundError
+            assert get_sudoers() == []
