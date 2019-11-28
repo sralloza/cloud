69f82be4eed746438cdeb56bb94d23caa2626e70
sralloza <sralloza@gmail.com>
2019-11-28 23:22:50 +0100

    Add changes in python code

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 import os
 import shutil
+from pathlib import Path
 
-from flask import Flask, request, render_template, redirect
+from flask import Flask, jsonify, make_response, redirect, render_template, request
 from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
 
 from .config import cfg
 from .forms import UploadForm
-from .utils import get_sudoers, get_user, log, get_folders
+from .utils import get_folders, get_sudoers, get_user, log
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -16,81 +17,86 @@ META = '<meta http-equiv="refresh" content="3;url=/files">'
 META2 = '<meta http-equiv="refresh" content="5;url=/files">'
 
 
-@app.route('/', methods=['GET'])
-def index():
-    form = UploadForm()
-
+@app.route("/")
+def new_route():
     folder_choices = get_folders()
-    form.folder.choices = [(i, x.as_posix()) for i, x in enumerate(folder_choices)]
 
-    log('User %r opened index', get_user())
-    return render_template('minimal.html', form=form)
+    log("User %r opened index", get_user())
+    return render_template("new.html", folders=folder_choices)
 
 
-@app.route('/upload', methods=['POST'])
+@app.route("/upload", methods=["POST"])
 def upload():
-    form = UploadForm()
+    print(request.form.to_dict(flat=False))
 
-    if form.folder.data is None:
-        return f'{META2}<h1>No folder supplied or an invalid folder was supplied<h1>', 400
+    file = request.form.get("file")
+    folder = request.form.get("folder")
 
-    folder_choices = get_folders()
+    folder_choices = [x.as_posix() for x in get_folders()]
 
-    try:
-        folder = folder_choices[int(form.folder.data)]
-    except IndexError:
-        return f'{META2}<h1>Invalid index folder<h2>', 400
-    files = request.files.getlist('files')
+    if not folder:
+        return {"message": "No folder supplied"}, 400
 
-    if not files:
-        return f'{META2}<h1>No files supplied</h1>', 400
+    folder = Path(folder).as_posix()
+    if folder not in folder_choices:
+        return {"message": "Invalid folder: %r" % folder}, 400
+    if not file:
+        return {"message": "No files supplied"}, 400
 
-    for f in files:
-        filename = secure_filename(f.filename)
-        filename = cfg.CLOUD_PATH / folder / filename
-        f.save(filename.as_posix())
+    save_file(file, folder)
+
+    return make_response(jsonify({"message": "File uploaded"}), 200)
+
+
+def save_file(file, folder):
+    filename = secure_filename(file.filename)
+    filename = cfg.CLOUD_PATH / folder / filename
+    file.save(filename.as_posix())
 
     log(
-        'User %r upload files to folder %r: %s', get_user(), folder,
-        [secure_filename(x.filename) for x in request.files.getlist('files')]
+        "User %r upload files to folder %r: %s",
+        get_user(),
+        folder,
+        secure_filename(file.filename)
+        # [secure_filename(x.filename) for x in request.files.getlist("files")],
     )
-    return redirect('/')
+    return redirect("/")
 
 
-@app.route('/d/<path:filepath>', methods=['GET'])
-@app.route('/delete/<path:filepath>', methods=['GET'])
+@app.route("/d/<path:filepath>", methods=["GET"])
+@app.route("/delete/<path:filepath>", methods=["GET"])
 def delete(filepath):
     filepath = cfg.CLOUD_PATH / filepath
 
     try:
         if filepath.is_dir():
             shutil.rmtree(filepath)
-            log('User %r removed tree %r', get_user(), filepath.as_posix())
-            return f'{META}<h1>Tree removed</h1> {filepath.as_posix()}', 200
+            log("User %r removed tree %r", get_user(), filepath.as_posix())
+            return f"{META}<h1>Tree removed</h1> {filepath.as_posix()}", 200
         else:
             os.remove(filepath)
-            log('User %r removed file %r', get_user(), filepath.as_posix())
-            return f'{META}<h1>File deleted</h1>  {filepath.as_posix()}', 200
+            log("User %r removed file %r", get_user(), filepath.as_posix())
+            return f"{META}<h1>File deleted</h1>  {filepath.as_posix()}", 200
     except FileNotFoundError:
-        log('User %r tried to incorrectly remove %r', get_user(), filepath.as_posix())
-        return f'{META2}<h1>File not found</h1> {filepath.as_posix()}', 404
+        log("User %r tried to incorrectly remove %r", get_user(), filepath.as_posix())
+        return f"{META2}<h1>File not found</h1> {filepath.as_posix()}", 404
 
 
-@app.route('/md/<path:folder>', methods=['GET'])
-@app.route('/mk/<path:folder>', methods=['GET'])
-@app.route('/mkdir/<path:folder>', methods=['GET'])
+@app.route("/md/<path:folder>", methods=["GET"])
+@app.route("/mk/<path:folder>", methods=["GET"])
+@app.route("/mkdir/<path:folder>", methods=["GET"])
 def mkdir(folder: str):
     os.makedirs(cfg.CLOUD_PATH / folder)
 
-    log('User %r made dir %r', get_user(), folder)
-    return redirect('/files')
+    log("User %r made dir %r", get_user(), folder)
+    return redirect("/files")
 
 
-@app.route('/mv', methods=['GET'])
-@app.route('/move', methods=['GET'])
+@app.route("/mv", methods=["GET"])
+@app.route("/move", methods=["GET"])
 def move():
-    _from = request.args.get('from')
-    _to = request.args.get('to')
+    _from = request.args.get("from")
+    _to = request.args.get("to")
 
     if not _from:
         log('User %r tried to move, but forgot "from" argument', get_user())
@@ -105,8 +111,14 @@ def move():
 
     try:
         shutil.move(real_from, real_to)
-        log('User %r moved file %r to %r', get_user(), _from, _to)
-        return f'{META}<h1>File moved correctly</h1>', 200
+        log("User %r moved file %r to %r", get_user(), _from, _to)
+        return f"{META}<h1>File moved correctly</h1>", 200
     except FileNotFoundError as err:
-        log('User %r tried to move file %r to %r, but failed (%r)', get_user(), _from, _to, err)
-        return f'{META2} File not found', 400
+        log(
+            "User %r tried to move file %r to %r, but failed (%r)",
+            get_user(),
+            _from,
+            _to,
+            err,
+        )
+        return f"{META2} File not found", 400
