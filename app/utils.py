29cccf903707559734945e72c57abcfffaf9a446
sralloza <sralloza@gmail.com>
2020-03-08 09:44:07 +0100

    Enable follow symlinks

--- app/utils.py
+++ app/utils.py
@@ -18,17 +18,17 @@ def get_sudoers():
         with cfg.SUDOERS_PATH.open() as f:
             return json.load(f)
     except json.JSONDecodeError as err:
-        warnings.warn(f'json decode error: {err}', SudoersWarning)
+        warnings.warn(f"json decode error: {err}", SudoersWarning)
         return []
     except FileNotFoundError:
-        warnings.warn('Sudoers file not found', SudoersWarning)
+        warnings.warn("Sudoers file not found", SudoersWarning)
         return []
 
 
 def log(string, *args):
-    timestamp = f'[{asctime()}] - {request.remote_addr} - '
-    with cfg.LOG_PATH.open('at') as f:
-        f.write(timestamp + string % args + '\n')
+    timestamp = "[%s] - %s - " % (asctime(), request.remote_addr)
+    with cfg.LOG_PATH.open("at") as f:
+        f.write(timestamp + string % args + "\n")
 
 
 def get_user():
@@ -40,13 +40,17 @@ def get_user():
 
 
 def get_folders():
-    folder_choices = [Path(x[0]).relative_to(cfg.CLOUD_PATH) for x in os.walk(cfg.CLOUD_PATH)]
+    folder_choices = [
+        Path(x[0]).relative_to(cfg.CLOUD_PATH)
+        for x in os.walk(cfg.CLOUD_PATH, followlinks=True)
+    ]
 
     if get_user() not in get_sudoers():
-        def filter_choice(x):
-            return not (x.as_posix().startswith('.') and len(x.as_posix()) > 1)
-
-        folder_choices = [x for x in folder_choices if filter_choice(x)]
+        folder_choices = [x for x in folder_choices if filter_non_admin_folders(x)]
 
     folder_choices.sort()
     return folder_choices
+
+
+def filter_non_admin_folders(x):
+    return not (x.as_posix().startswith(".") and len(x.as_posix()) > 1)
