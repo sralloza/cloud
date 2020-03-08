73fda3a936001aa908eec09466794b54390f9e2b
sralloza <sralloza@gmail.com>
2020-03-08 11:45:55 +0100

    Add config HIDE_PATH

--- app/config.py
+++ app/config.py
@@ -1,12 +1,13 @@
 import platform
 from pathlib import Path
-
+import json
 
 class _Config:
-    LOG_PATH = Path('')
-    CLOUD_PATH = Path('')
-    SUDOERS_PATH = Path('')
-    PLATFORM = ''
+    LOG_PATH = Path("")
+    CLOUD_PATH = Path("")
+    SUDOERS_PATH = Path("")
+    PLATFORM = ""
+    HIDE_PATH = Path("")
 
     @staticmethod
     def setup_config():
@@ -14,23 +15,28 @@ class _Config:
         cfg.CLOUD_PATH.mkdir(exist_ok=True)
         cfg.SUDOERS_PATH.touch()
 
+        if not cfg.HIDE_PATH.exists():
+            cfg.HIDE_PATH.write_text(json.dumps(list()))
+
 
 class _Windows(_Config):
-    LOG_PATH = Path('D:\\.scripts\\cloud\\web.log')
-    CLOUD_PATH = Path('D:\\.scripts\\cloud\\files')
-    SUDOERS_PATH = Path('D:\\.scripts\\cloud\\sudoers.json')
-    PLATFORM = 'Windows'
+    LOG_PATH = Path("D:\\.scripts\\cloud\\web.log")
+    CLOUD_PATH = Path("D:\\.scripts\\cloud\\files")
+    SUDOERS_PATH = Path("D:\\.scripts\\cloud\\sudoers.json")
+    HIDE_PATH = Path("D:\\.scripts\\cloud\\hide.json")
+    PLATFORM = "Windows"
 
 
 class _Linux(_Config):
-    LOG_PATH = Path('/srv/cloud/web.log')
-    CLOUD_PATH = Path('/srv/cloud/files')
-    SUDOERS_PATH = Path('/srv/cloud/sudoers.json')
-    PLATFORM = 'Linux'
+    LOG_PATH = Path("/srv/cloud/web.log")
+    CLOUD_PATH = Path("/srv/cloud/files")
+    SUDOERS_PATH = Path("/srv/cloud/sudoers.json")
+    HIDE_PATH = Path("/srv/cloud/hide.json")
+    PLATFORM = "Linux"
 
 
 def get_current_config():
-    if platform.system() == 'Linux':
+    if platform.system() == "Linux":
         return _Linux()
     else:
         return _Windows()
