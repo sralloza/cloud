cedbb6c82657e06427a03b28aad202a770507efe
sralloza <sralloza@gmail.com>
2020-03-08 11:51:34 +0100

    Generate random password as app's secret key

--- run_windows.py
+++ run_windows.py
@@ -1,6 +1,7 @@
 from app import app
+from app.utils import gen_random_password
 
 
-if __name__ == '__main__':
-    app.secret_key = 'asdfpoiuwelkrj123ñ53543'
+if __name__ == "__main__":
+    app.secret_key = gen_random_password()
     app.run(port=80, debug=True)
