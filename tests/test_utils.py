86972001a5a725944e9f40d13c3201077ac0eafa
sralloza <sralloza@gmail.com>
2020-03-08 12:04:32 +0100

    Update tests of get_user

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,40 +9,40 @@ from app.utils import SudoersWarning, get_sudoers, log, get_user, get_folders
 
 def test_warning():
     with pytest.warns(SudoersWarning):
-        warnings.warn('Sudoers Warning', SudoersWarning)
+        warnings.warn("Sudoers Warning", SudoersWarning)
 
 
-@mock.patch('app.utils.cfg')
+@mock.patch("app.utils.cfg")
 def test_get_sudoers(mocker):
     fp = mocker.SUDOERS_PATH.open.return_value.__enter__.return_value
     fp.read.return_value = '["user_a"]'
-    assert get_sudoers() == ['user_a']
+    assert get_sudoers() == ["user_a"]
 
-    with pytest.warns(SudoersWarning, match='json decode error'):
-        fp.read.return_value = 'invalid'
+    with pytest.warns(SudoersWarning, match="json decode error"):
+        fp.read.return_value = "invalid"
         assert get_sudoers() == []
 
-    with pytest.warns(SudoersWarning, match='Sudoers file not found'):
+    with pytest.warns(SudoersWarning, match="Sudoers file not found"):
         fp.read.side_effect = FileNotFoundError
         assert get_sudoers() == []
 
 
-@mock.patch('app.utils.cfg', spec=True)
-@mock.patch('app.utils.request', spec=True)
-@mock.patch('app.utils.asctime', spec=True)
+@mock.patch("app.utils.cfg", spec=True)
+@mock.patch("app.utils.request", spec=True)
+@mock.patch("app.utils.asctime", spec=True)
 def test_log(asctime_mock, request_mock, cfg_mock):
     str_time = datetime(2019, 1, 1).ctime()
     asctime_mock.return_value = str_time
-    request_mock.remote_addr = '10.0.0.1'
+    request_mock.remote_addr = "10.0.0.1"
     fp = cfg_mock.LOG_PATH.open.return_value.__enter__.return_value
 
-    args = f'[{str_time}] - 10.0.0.1 - hello-world\n'
+    args = f"[{str_time}] - 10.0.0.1 - hello-world\n"
 
-    log('hello-world')
+    log("hello-world")
     asctime_mock.assert_called_once()
     fp.write.assert_called_once_with(args)
 
-    log('a=%s, b=%r, c=%03d', 'letter-a', 'letter-b', 10)
+    log("a=%s, b=%r, c=%03d", "letter-a", "letter-b", 10)
     args = f"[{str_time}] - 10.0.0.1 - a=letter-a, b='letter-b', c=010\n"
 
     assert asctime_mock.call_count == 2
@@ -50,21 +50,29 @@ def test_log(asctime_mock, request_mock, cfg_mock):
     assert fp.write.call_count == 2
 
 
-@mock.patch('app.utils.request', spec=True)
-def test_get_user(request_mock):
-    request_mock.authorization = None
-    assert get_user() is None
+@mock.patch("app.utils.request", spec=True)
+class TestGetUser:
+    def test_no_authorization(self, request_mock):
+        request_mock.authorization = None
+        assert get_user() is None
 
-    request_mock.authorization = mock.Mock(username='foo')
-    assert get_user() == 'foo'
+    def test_normal_authorization(self, request_mock):
+        request_mock.authorization = mock.Mock(username="foo")
+        assert get_user() == "foo"
+
+    def test_runtime_error(self, request_mock):
+        prop_mock = mock.PropertyMock()
+        prop_mock.side_effect = RuntimeError
+        type(request_mock).authorization = prop_mock
+        assert get_user() is None
 
 
 @pytest.fixture
 def folders_mocker():
-    cfg_mock = mock.patch('app.utils.cfg', spec=True).start()
-    os_walk_mock = mock.patch('os.walk', spec=True).start()
-    get_user_mock = mock.patch('app.utils.get_user', spec=True).start()
-    get_sudoers_mock = mock.patch('app.utils.get_sudoers', spec=True).start()
+    cfg_mock = mock.patch("app.utils.cfg", spec=True).start()
+    os_walk_mock = mock.patch("os.walk", spec=True).start()
+    get_user_mock = mock.patch("app.utils.get_user", spec=True).start()
+    get_sudoers_mock = mock.patch("app.utils.get_sudoers", spec=True).start()
 
     yield cfg_mock, os_walk_mock, get_user_mock, get_sudoers_mock
 
@@ -74,18 +82,29 @@ def folders_mocker():
 def test_get_folders(folders_mocker):
     cfg_mock, os_walk_mock, get_user_mock, get_sudoers_mock = folders_mocker
 
-    cfg_mock.CLOUD_PATH = '/test'
+    cfg_mock.CLOUD_PATH = "/test"
     os_walk_mock.return_value = [
-        ('/test/folder-1',), ('/test/folder-1/subfolder-1.2/subsubfolder-1.2.1',),
-        ('/test/folder-1/subfolder-1.1',), ('/test/folder-1/subfolder-1.2',),
-        ('/test/folder-2',), ('/test/folder-2/subfolder-2.1',)
+        ("/test/folder-1",),
+        ("/test/folder-1/subfolder-1.2/subsubfolder-1.2.1",),
+        ("/test/folder-1/subfolder-1.1",),
+        ("/test/folder-1/subfolder-1.2",),
+        ("/test/folder-2",),
+        ("/test/folder-2/subfolder-2.1",),
     ]
     get_user_mock.return_value = None
     get_sudoers_mock.return_value = []
 
     expected = [
-        'folder-1', 'folder-1/subfolder-1.1', 'folder-1/subfolder-1.2',
-        'folder-1/subfolder-1.2/subsubfolder-1.2.1', 'folder-2', 'folder-2/subfolder-2.1'
+        "folder-1",
+        "folder-1/subfolder-1.1",
+        "folder-1/subfolder-1.2",
+        "folder-1/subfolder-1.2/subsubfolder-1.2.1",
+        "folder-2",
+        "folder-2/subfolder-2.1",
     ]
 
     assert [x.as_posix() for x in get_folders()] == expected
+
+
+def test_gen_random_password():
+    assert 0, "Not implemented"
