d7a2dabd1d79904219ae3f1d0996e397c233679e
sralloza <sralloza@gmail.com>
2020-03-09 22:42:37 +0100

    Add tests for /show-hides endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -505,6 +505,16 @@ def test_hide(add_hides_mock, client):
     assert rv.status_code == 200
 
 
+@mock.patch("app.get_hides")
+def test_show_hides(get_hides_mock, client):
+    get_hides_mock.return_value = ["a", "b", "c", "d"]
+    rv = client.get("/show-hides")
+
+    get_hides_mock.assert_called_once()
+    assert rv.status_code == 200
+    assert rv.data == b"a<br>b<br>c<br>d"
+
+
 @mock.patch("app.remove_from_hides")
 def test_unhide(remove_hide_mock, client):
     rv = client.get("/unhide/path/to/folder")
