573e6544ca64fcfc0062a1e6bf7fdd4e88cd9fea
sralloza <sralloza@gmail.com>
2020-03-10 19:01:01 +0100

    Fix conftest to use factory create_app

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,18 @@
 import pytest
 
-from app import app
+from app import create_app
 
 
-@pytest.fixture(scope='module')
-def client():
-    app.secret_key = 'asdfjewpoirjopfnñcxvkjs0i49309'
+@pytest.fixture(scope="module")
+def client(app):
+    app.secret_key = "secret-known"
     testing_client = app.test_client()
-
 
     with app.app_context():
         yield testing_client
 
-@pytest.fixture(scope='module')
-def create_app():
-    app.secret_key = 'asdfjewpoirjopfnñcxvkjs0i49309'
-    yield app
+
+@pytest.fixture(scope="module")
+def app():
+    application = create_app()
+    yield application
