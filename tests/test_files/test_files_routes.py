419d258e7a216fe2d42380d597917082ab6bf8d6
sralloza <sralloza@gmail.com>
2020-03-11 18:10:54 +0100

    Fix tests for /upload

--- tests/test_files/test_files_routes.py
+++ tests/test_files/test_files_routes.py
@@ -24,168 +24,208 @@ class TestUpload:
         folders = [Path(x) for x in ["folder-1", "folder-2", "folder-3"]]
 
         cfg_mock = mock.patch("app.files.routes.cfg").start()
-        file_storage_mock = mock.patch(
-            "werkzeug.datastructures.FileStorage.save"
-        ).start()
-        get_folders_mock = mock.patch("app.files.routes.get_folders").start()
+        save_mock = mock.patch("werkzeug.datastructures.FileStorage.save").start()
+        folders_mock = mock.patch("app.files.routes.get_folders").start()
         log_mock = mock.patch("app.files.routes.log").start()
-        get_user_mock = mock.patch("app.files.routes.get_user").start()
+        gu_mock = mock.patch("app.files.routes.get_user").start()
 
         cfg_mock.CLOUD_PATH = Path("/cloud")
-        get_folders_mock.return_value = folders
-        get_user_mock.return_value = "user-foo"
+        folders_mock.return_value = folders
+        gu_mock.return_value = "user-foo"
 
-        yield cfg_mock, file_storage_mock, get_folders_mock, log_mock, get_user_mock
+        yield save_mock, folders_mock, log_mock, gu_mock
 
         mock.patch.stopall()
 
     def test_one_file(self, client, upload_mocks):
-        _, file_storage_mock, _, _, _ = upload_mocks
+        save_mock, folders_mock, log_mock, gu_mock = upload_mocks
 
         rv = client.post(
             "/upload",
-            data=dict(
-                files=[(io.BytesIO(b"this is a test"), "test.pdf")],
-                folder=0,
-                submit="Upload",
-            ),
+            data={
+                "files[]": [(io.BytesIO(b"this is a test"), "test.pdf")],
+                "folder": 0,
+                "submit": "Upload",
+            },
             follow_redirects=True,
         )
 
         assert rv.status_code == 200
-        file_storage_mock.assert_called_once_with("/cloud/folder-1/test.pdf")
-        assert file_storage_mock.call_count == 1
+        folders_mock.assert_called_once()
+
+        log_mock.assert_called()
+        gu_mock.assert_called()
+        assert log_mock.call_count == 2
+        assert gu_mock.call_count == 2
+
+        save_mock.assert_called_once_with("/cloud/folder-1/test.pdf")
+        assert save_mock.call_count == 1
 
         rv = client.post(
             "/upload",
-            data=dict(
-                files=[(io.BytesIO(b"this is a test"), "test.rar")],
-                folder=2,
-                submit="Upload",
-            ),
+            data={
+                "files[]": [(io.BytesIO(b"this is a test"), "test.rar")],
+                "folder": 2,
+                "submit": "Upload",
+            },
             follow_redirects=True,
         )
 
         assert rv.status_code == 200
-        file_storage_mock.assert_called_with("/cloud/folder-3/test.rar")
-        assert file_storage_mock.call_count == 2
+        folders_mock.assert_called()
+        assert folders_mock.call_count == 2
+
+        log_mock.assert_called()
+        gu_mock.assert_called()
+        assert log_mock.call_count == 4
+        assert gu_mock.call_count == 4
+
+        save_mock.assert_called_with("/cloud/folder-3/test.rar")
+        assert save_mock.call_count == 2
 
     def test_multiple_files(self, client, upload_mocks):
-        _, file_storage_mock, _, _, _ = upload_mocks
+        save_mock, folders_mock, log_mock, gu_mock = upload_mocks
 
-        file_storage_mock.reset_mock()
         rv = client.post(
             "/upload",
-            data=dict(
-                files=[
+            data={
+                "files[]": [
                     (io.BytesIO(b"this is a test"), "test-1.py"),
                     (io.BytesIO(b"this is a test"), "test-2.py"),
                     (io.BytesIO(b"this is a test"), "test-3.py"),
                 ],
-                folder=2,
-                submit="Upload",
-            ),
+                "folder": 2,
+                "submit": "Upload",
+            },
             follow_redirects=True,
         )
 
         assert rv.status_code == 200
-        file_storage_mock.assert_has_calls(
-            [
-                mock.call("/cloud/folder-3/test-1.py"),
-                mock.call("/cloud/folder-3/test-2.py"),
-                mock.call("/cloud/folder-3/test-3.py"),
-            ]
-        )
 
-        assert file_storage_mock.call_count == 3
+        save_mock.assert_any_call("/cloud/folder-3/test-1.py")
+        save_mock.assert_any_call("/cloud/folder-3/test-2.py")
+        save_mock.assert_any_call("/cloud/folder-3/test-3.py")
+        assert save_mock.call_count == 3
+
+        folders_mock.assert_called_once()
+        log_mock.assert_called()
+        assert log_mock.call_count == 2
+        gu_mock.assert_called()
+        assert gu_mock.call_count == 2
 
     def test_no_files(self, client, upload_mocks):
-        _, file_storage_mock, _, _, _ = upload_mocks
-
-        file_storage_mock.reset_mock()
-        rv = client.post(
-            "/upload",
-            data=dict(files=[], folder=2, submit="Upload"),
-            follow_redirects=True,
-        )
-
-        assert rv.status_code == 400
-        assert b"No files supplied" in rv.data
-        file_storage_mock.assert_not_called()
-
-    def test_folder_as_str(self, client, upload_mocks):
-        _, file_storage_mock, _, _, _ = upload_mocks
+        save_mock, folders_mock, log_mock, gu_mock = upload_mocks
 
         rv = client.post(
             "/upload",
-            data=dict(
-                files=[(io.BytesIO(b"this is a test"), "whatever.rar")],
-                folder="2",
-                submit="Upload",
-            ),
+            data={"files[]": [], "folder": 2, "submit": "Upload"},
             follow_redirects=True,
         )
 
         assert rv.status_code == 200
-        file_storage_mock.assert_called_once_with("/cloud/folder-3/whatever.rar")
+        assert b"No files supplied" in rv.data
+        save_mock.assert_not_called()
+
+        folders_mock.assert_called_once()
+        log_mock.assert_called_once()
+        gu_mock.assert_called_once()
+
+    def test_folder_as_str(self, client, upload_mocks):
+        save_mock, folders_mock, log_mock, gu_mock = upload_mocks
+
+        rv = client.post(
+            "/upload",
+            data={
+                "files[]": [(io.BytesIO(b"this is a test"), "whatever.rar")],
+                "folder": "2",
+                "submit": "Upload",
+            },
+            follow_redirects=True,
+        )
+
+        assert rv.status_code == 200
+        save_mock.assert_called_once_with("/cloud/folder-3/whatever.rar")
+        folders_mock.assert_called_once()
+
+        log_mock.assert_called()
+        gu_mock.assert_called()
+        assert log_mock.call_count == 2
+        assert gu_mock.call_count == 2
 
     def test_no_folder(self, client, upload_mocks):
-        _, file_storage_mock, _, _, _ = upload_mocks
+        save_mock, folders_mock, log_mock, gu_mock = upload_mocks
 
         rv = client.post(
             "/upload",
-            data=dict(
-                files=[(io.BytesIO(b"this is a test"), "whatever.pdf")], submit="Upload"
-            ),
+            data={
+                "files[]": [(io.BytesIO(b"this is a test"), "whatever.pdf")],
+                "submit": "Upload",
+            },
             follow_redirects=True,
         )
 
-        assert rv.status_code == 400
+        assert rv.status_code == 200
         assert b"No folder supplied" in rv.data
-        file_storage_mock.assert_not_called()
+        save_mock.assert_not_called()
+        folders_mock.assert_not_called()
+
+        log_mock.assert_called_once()
+        gu_mock.assert_called_once()
 
     def test_type_invalid_folder(self, client, upload_mocks):
-        _, file_storage_mock, _, _, _ = upload_mocks
+        save_mock, folders_mock, log_mock, gu_mock = upload_mocks
 
         rv = client.post(
             "/upload",
-            data=dict(
-                files=[(io.BytesIO(b"this is a test"), "whatever.rar")],
-                folder="-",
-                submit="Upload",
-            ),
+            data={
+                "files[]": [(io.BytesIO(b"this is a test"), "whatever.rar")],
+                "folder": "-",
+                "submit": "Upload",
+            },
             follow_redirects=True,
         )
 
-        assert rv.status_code == 400
-        assert b"No folder supplied or an invalid folder was supplied" in rv.data
-        file_storage_mock.assert_not_called()
+        assert rv.status_code == 200
+        assert b"Invalid index folder" in rv.data
+        save_mock.assert_not_called()
+        folders_mock.assert_called_once()
+        log_mock.assert_called_once()
+        gu_mock.assert_called_once()
 
     def test_index_invalid_folder(self, client, upload_mocks):
-        _, file_storage_mock, _, _, _ = upload_mocks
+        save_mock, folders_mock, log_mock, gu_mock = upload_mocks
 
         rv = client.post(
             "/upload",
-            data=dict(
-                files=[(io.BytesIO(b"this is a test"), "whatever.rar")],
-                folder=65,
-                submit="Upload",
-            ),
+            data={
+                "files[]": [(io.BytesIO(b"this is a test"), "whatever.rar")],
+                "folder": 99999999999,
+                "submit": "Upload",
+            },
             follow_redirects=True,
         )
 
-        assert rv.status_code == 400
+        assert rv.status_code == 200
         assert b"Invalid index folder" in rv.data
-        file_storage_mock.assert_not_called()
+        save_mock.assert_not_called()
+        folders_mock.assert_called_once()
+        log_mock.assert_called_once()
+        gu_mock.assert_called_once()
 
     def test_methods(self, client, upload_mocks):
-        rv = client.post("/upload")
-        assert rv.status_code in [200, 400]
+        save_mock, folders_mock, log_mock, gu_mock = upload_mocks
 
+        assert client.post("/upload").status_code == 302
+        assert client.post("/upload", follow_redirects=True).status_code == 200
         assert client.get("/upload").status_code == 405
         assert client.put("/upload").status_code == 405
         assert client.delete("/upload").status_code == 405
         assert client.patch("/upload").status_code == 405
+
+        save_mock.assert_not_called()
+        folders_mock.assert_not_called()
+        log_mock.assert_called()
+        gu_mock.assert_called()
 
 
 class TestDelete:
