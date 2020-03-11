87f1001330925d4351db419038e02d446647e3c8
sralloza <sralloza@gmail.com>
2020-03-11 18:29:06 +0100

    Improve tests for /move

--- tests/test_files/test_files_routes.py
+++ tests/test_files/test_files_routes.py
@@ -130,6 +130,25 @@ class TestUpload:
         log_mock.assert_called_once()
         gu_mock.assert_called_once()
 
+    @pytest.mark.parametrize("nfiles", range(1, 4))
+    def test_empty_file(self, client, upload_mocks, nfiles):
+        save_mock, folders_mock, log_mock, gu_mock = upload_mocks
+
+        files = [(io.BytesIO(b""), "")] * nfiles
+        rv = client.post(
+            "/upload",
+            data={"files[]": files, "folder": 2, "submit": "Upload",},
+            follow_redirects=True,
+        )
+
+        assert rv.status_code == 200
+        assert b"Supplied only %d emtpy files" % nfiles in rv.data
+        save_mock.assert_not_called()
+
+        folders_mock.assert_called_once()
+        log_mock.assert_called_once()
+        gu_mock.assert_called_once()
+
     def test_folder_as_str(self, client, upload_mocks):
         save_mock, folders_mock, log_mock, gu_mock = upload_mocks
 
@@ -235,13 +254,13 @@ class TestDelete:
         cfg_mock = mock.patch("app.files.routes.cfg").start()
         rmtree_mock = mock.patch("shutil.rmtree").start()
         is_dir_mock = mock.patch("pathlib.Path.is_dir").start()
-        get_user_mock = mock.patch("app.files.routes.get_user").start()
+        gu_mock = mock.patch("app.files.routes.get_user").start()
         log_mock = mock.patch("app.files.routes.log").start()
 
         cfg_mock.CLOUD_PATH = Path("/cloud")
-        get_user_mock.return_value = "user-foo"
+        gu_mock.return_value = "user-foo"
 
-        yield remove_mock, cfg_mock, rmtree_mock, is_dir_mock, get_user_mock, log_mock
+        yield remove_mock, cfg_mock, rmtree_mock, is_dir_mock, gu_mock, log_mock
 
         mock.patch.stopall()
 
@@ -250,7 +269,7 @@ class TestDelete:
         return request.param
 
     def test_delete_file(self, delete_mocks, client, filepath):
-        remove_mock, _, rmtree_mock, is_dir_mock, get_user_mock, log_mock = delete_mocks
+        remove_mock, _, rmtree_mock, is_dir_mock, gu_mock, log_mock = delete_mocks
 
         is_dir_mock.return_value = False
 
@@ -263,7 +282,7 @@ class TestDelete:
         is_dir_mock.assert_called_once()
         remove_mock.assert_called_once_with(delete_path)
         rmtree_mock.assert_not_called()
-        get_user_mock.assert_called_once_with()
+        gu_mock.assert_called_once_with()
         log_mock.assert_called_once_with(
             "User %r removed file %r", "user-foo", delete_path.as_posix()
         )
@@ -272,7 +291,7 @@ class TestDelete:
         assert delete_path.as_posix().encode() in rv.data
 
     def test_delete_folder(self, delete_mocks, client, filepath):
-        remove_mock, _, rmtree_mock, is_dir_mock, get_user_mock, log_mock = delete_mocks
+        remove_mock, _, rmtree_mock, is_dir_mock, gu_mock, log_mock = delete_mocks
         is_dir_mock.return_value = True
 
         url = f"/d/{filepath}/"
@@ -284,7 +303,7 @@ class TestDelete:
         is_dir_mock.assert_called_once()
         remove_mock.assert_not_called()
         rmtree_mock.assert_called_once_with(delete_path)
-        get_user_mock.assert_called_once_with()
+        gu_mock.assert_called_once_with()
         log_mock.assert_called_once_with(
             "User %r removed tree %r", "user-foo", delete_path.as_posix()
         )
@@ -293,7 +312,7 @@ class TestDelete:
         assert delete_path.as_posix().encode() in rv.data
 
     def test_delete_non_existing_file(self, delete_mocks, client, filepath):
-        remove_mock, _, rmtree_mock, is_dir_mock, get_user_mock, log_mock = delete_mocks
+        remove_mock, _, rmtree_mock, is_dir_mock, gu_mock, log_mock = delete_mocks
         is_dir_mock.return_value = False
         remove_mock.side_effect = FileNotFoundError
 
@@ -306,7 +325,7 @@ class TestDelete:
         is_dir_mock.assert_called_once()
         remove_mock.assert_called_once_with(delete_path)
         rmtree_mock.assert_not_called()
-        get_user_mock.assert_called_once_with()
+        gu_mock.assert_called_once_with()
         log_mock.assert_called_once_with(
             "User %r tried to incorrectly remove %r", "user-foo", delete_path.as_posix()
         )
@@ -315,7 +334,7 @@ class TestDelete:
         assert delete_path.as_posix().encode() in rv.data
 
     def test_delete_non_existing_folder(self, delete_mocks, client, filepath):
-        remove_mock, _, rmtree_mock, is_dir_mock, get_user_mock, log_mock = delete_mocks
+        remove_mock, _, rmtree_mock, is_dir_mock, gu_mock, log_mock = delete_mocks
         is_dir_mock.return_value = True
         rmtree_mock.side_effect = FileNotFoundError
 
@@ -328,7 +347,7 @@ class TestDelete:
         is_dir_mock.assert_called_once()
         remove_mock.assert_not_called()
         rmtree_mock.assert_called_once_with(delete_path)
-        get_user_mock.assert_called_once_with()
+        gu_mock.assert_called_once_with()
         log_mock.assert_called_once_with(
             "User %r tried to incorrectly remove %r", "user-foo", delete_path.as_posix()
         )
@@ -343,12 +362,12 @@ class TestMkdir:
         makedirs_mock = mock.patch("os.makedirs").start()
         cfg_mock = mock.patch("app.files.routes.cfg").start()
         log_mock = mock.patch("app.files.routes.log").start()
-        get_user_mock = mock.patch("app.files.routes.get_user").start()
+        gu_mock = mock.patch("app.files.routes.get_user").start()
 
         cfg_mock.CLOUD_PATH = Path("/cloud")
-        get_user_mock.return_value = "user-foo"
+        gu_mock.return_value = "user-foo"
 
-        yield makedirs_mock, cfg_mock, log_mock, get_user_mock
+        yield makedirs_mock, cfg_mock, log_mock, gu_mock
         mock.patch.stopall()
 
     @pytest.fixture(params=FILEPATHS)
@@ -356,7 +375,7 @@ class TestMkdir:
         return request.param
 
     def test_mkdir(self, client, mkdir_mocks, filepath):
-        makedirs_mock, cfg_mock, log_mock, get_user_mock = mkdir_mocks
+        makedirs_mock, cfg_mock, log_mock, gu_mock = mkdir_mocks
 
         url = f"/mkdir/{filepath}"
         client.get(url)
@@ -365,24 +384,24 @@ class TestMkdir:
 
         makedirs_mock.assert_called_once_with(make_path)
         log_mock.assert_called_once_with("User %r made dir %r", "user-foo", filepath)
-        get_user_mock.assert_called_once_with()
+        gu_mock.assert_called_once_with()
 
 
 class TestMove:
     @pytest.fixture
     def move_mocks(self):
         log_mock = mock.patch("app.files.routes.log").start()
-        get_user_mock = mock.patch(
+        gu_mock = mock.patch(
             "app.files.routes.get_user", return_value="user-bar"
         ).start()
         move_mock = mock.patch("shutil.move").start()
 
-        yield log_mock, get_user_mock, move_mock
+        yield log_mock, gu_mock, move_mock
         mock.patch.stopall()
 
     @pytest.mark.parametrize("_to", FILEPATHS)
     def test_without_from(self, move_mocks, client, _to):
-        log_mock, get_user_mock, move_mock = move_mocks
+        log_mock, gu_mock, move_mock = move_mocks
 
         mv_url = f"/mv?to={_to}"
         rv = client.get(mv_url)
@@ -391,11 +410,13 @@ class TestMove:
         log_mock.assert_called_once_with(
             'User %r tried to move, but forgot "from" argument', "user-bar"
         )
+        gu_mock.assert_called_once()
+        move_mock.assert_not_called()
         assert b'Missing "from" argument' in rv.data
 
     @pytest.mark.parametrize("_from", FILEPATHS)
     def test_without_to(self, move_mocks, client, _from):
-        log_mock, get_user_mock, move_mock = move_mocks
+        log_mock, gu_mock, move_mock = move_mocks
 
         move_url = f"/mv?from={_from}"
         rv = client.get(move_url)
@@ -404,11 +425,13 @@ class TestMove:
         log_mock.assert_called_once_with(
             'User %r tried to move, but forgot "to" argument', "user-bar"
         )
+        gu_mock.assert_called_once()
+        move_mock.assert_not_called()
         assert b'Missing "to" argument' in rv.data
 
     @pytest.mark.parametrize("_to, _from", product(FILEPATHS, FILEPATHS))
     def test_move_ok(self, move_mocks, client, _to, _from):
-        log_mock, get_user_mock, move_mock = move_mocks
+        log_mock, gu_mock, move_mock = move_mocks
 
         move_url = f"/mv?from={_from}&to={_to}"
         rv = client.get(move_url)
@@ -417,4 +440,20 @@ class TestMove:
         log_mock.assert_called_once_with(
             "User %r moved file %r to %r", "user-bar", _from, _to
         )
+        gu_mock.assert_called_once()
+        move_mock.assert_called_once()
         assert b"File moved correctly" in rv.data
+
+    @pytest.mark.parametrize("exc", [FileNotFoundError, FileExistsError])
+    @pytest.mark.parametrize("_to, _from", product(FILEPATHS, FILEPATHS))
+    def test_move_error(self, move_mocks, client, _to, _from, exc):
+        log_mock, gu_mock, move_mock = move_mocks
+        move_mock.side_effect = exc
+
+        move_url = f"/mv?from={_from}&to={_to}"
+        rv = client.get(move_url)
+        assert rv.status_code == 400
+
+        log_mock.assert_called_once()
+        gu_mock.assert_called_once()
+        assert b"File not found" in rv.data
