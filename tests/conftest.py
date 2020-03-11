ab97ad6e89328e6c6665c7b3054bbf8992481477
sralloza <sralloza@gmail.com>
2020-03-11 18:53:05 +0100

    Make app fixture used automatically

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,12 @@ import pytest
 from app import create_app
 
 
+@pytest.fixture(scope="session", autouse=True)
+def app():
+    application = create_app()
+    yield application
+
+
 @pytest.fixture(scope="module")
 def client(app):
     app.secret_key = "secret-known"
@@ -10,9 +16,3 @@ def client(app):
 
     with app.app_context():
         yield testing_client
-
-
-@pytest.fixture(scope="module")
-def app():
-    application = create_app()
-    yield application
